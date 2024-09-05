eb4aa012972bdd6b08d482059ef0258e03e2c98f
Esteban Acevedo <tucorreo@correo>
2024-09-04 21:09:15 -0300

    feat: se puede modificar el perfil

--- proyecto/core/forms.py
+++ proyecto/core/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -6,3 +7,9 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'password1', 'password2')
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
--- proyecto/core/views.py
+++ proyecto/core/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, UserProfileForm
 
 
 def index(request):
@@ -13,3 +15,14 @@ class Register(CreateView):
     form_class = CustomUserCreationForm
     template_name = 'core/register.html'
     success_url = reverse_lazy('core:login')
+
+
+class Profile(LoginRequiredMixin, UpdateView):
+    model = User
+    form_class = UserProfileForm
+    template_name = 'core/profile.html'
+    success_url = reverse_lazy('core:index')
+
+    def get_object(self):
+        # Devuelve el usuario actual, si no, Django va a esperar un pk en la URL
+        return self.request.user
