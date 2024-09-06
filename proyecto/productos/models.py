44f9cb07a6973f979dfb494509886703e4de36cd
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2024-09-06 12:57:01 -0300

    feat: mejoras en modelo Venta y Producto

--- proyecto/productos/models.py
+++ proyecto/productos/models.py
@@ -26,7 +26,7 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField(blank=True, null=True)
     unidad_de_medida = models.CharField(max_length=50)
-    stock = models.FloatField()
+    stock = models.DecimalField(max_digits=10, decimal_places=2)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     fecha_actualizacion = models.DateTimeField(editable=False, auto_now=True)
 
@@ -36,4 +36,22 @@ class Producto(models.Model):
     class Meta:
         verbose_name = 'Producto'
         verbose_name_plural = 'Productos'
-        unique_together = ('categoria', 'nombre')
+        # La siguiente línea es para que no se puedan repetir el nombre y la categoría
+        constraints = [
+            models.UniqueConstraint(fields=['categoria', 'nombre'], name='categoria_nombre')
+        ]
+        # La siguiente línea es para crear un índice en la base de datos
+        # Un ínidice es una estructura de datos que mejora la velocidad de búsqueda
+        indexes = [models.Index(fields=['nombre'])]
+
+    def disminuir_stock(self, cantidad):
+        """cantidad es enviado desde el modelo Venta"""
+        if self.stock >= cantidad:
+            self.stock -= cantidad
+            self.save()
+        else:
+            raise ValueError('No hay suficiente stock disponible')
+
+    def aumentar_stock(self, cantidad):
+        self.stock += cantidad
+        self.save()
--- proyecto/ventas/models.py
+++ proyecto/ventas/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator
 from django.db import models
-from django.utils import timezone
 
 
 class Vendedor(models.Model):
@@ -18,19 +18,33 @@ class Vendedor(models.Model):
 
 
 class Venta(models.Model):
+    id: int
     vendedor = models.ForeignKey(Vendedor, on_delete=models.DO_NOTHING)
     producto = models.ForeignKey('productos.Producto', on_delete=models.DO_NOTHING)
-    cantidad = models.PositiveIntegerField()
+    cantidad = models.DecimalField(
+        max_digits=10, decimal_places=2, validators=[MinValueValidator(0)]
+    )
     precio_total = models.DecimalField(max_digits=10, decimal_places=2, editable=False)
-    fecha_venta = models.DateTimeField(default=timezone.now, editable=False)
+    fecha_venta = models.DateTimeField(auto_now_add=True, editable=False)
+
+    def __str__(self):
+        return f'Venta {self.id} - {self.producto.nombre} x {self.cantidad}'
 
     class Meta:
         ordering = ['-fecha_venta']
 
     def clean(self):
+        if not self.producto:
+            raise ValidationError('El producto no existe')
+        if self.producto.stock == 0:
+            raise ValidationError('No hay stock disponible para este producto')
         if self.cantidad > self.producto.stock:
-            raise ValidationError('La cantidad de venta no puede ser mayor al stock disponible.')
+            raise ValidationError(
+                f'No hay suficiente stock disponible para vender {self.cantidad} unidades.'
+                f'Solo hay {self.producto.stock} unidades disponibles.'
+            )
 
     def save(self, *args, **kwargs):
         self.precio_total = self.producto.precio * self.cantidad
+        self.producto.disminuir_stock(self.cantidad)
         super().save(*args, **kwargs)
